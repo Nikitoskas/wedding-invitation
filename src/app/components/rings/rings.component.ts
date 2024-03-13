e190962b8d1cbd4d2d1d328fffb6ee5618fbeca4
Nikita Kireev <1998nikitka@gmail.com>
2024-03-13 23:43:17 +0300

    work

--- src/app/components/rings/rings.component.ts
+++ src/app/components/rings/rings.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Subject, interval, takeUntil } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-rings',
   templateUrl: './rings.component.html',
   styleUrls: ['./rings.component.sass']
 })
-export class RingsComponent implements OnInit {
+export class RingsComponent implements OnInit, OnDestroy {
+
+  rotateLeft: number = 0
+  rotateRight: number = 0
+  step: number = 3
 
   constructor() { }
 
   ngOnInit() {
+    interval(200)
+    .pipe(takeUntil(this.onDestroy$))
+    .subscribe(e => {
+      this.rotateLeft = (this.rotateLeft + this.step)
+      this.rotateRight = (this.rotateRight - this.step)
+    })
   }
+
+  onDestroy$ = new Subject();
+  ngOnDestroy(): void {
+    this.onDestroy$.next(undefined);
+    this.onDestroy$.complete();
+  }
+
+
 
 }
